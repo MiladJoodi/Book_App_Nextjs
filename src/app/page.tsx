5eb3c5cb0d1f50219f8f8162d7ecab59963136eb
Milad Joodi <winbeta.com@gmail.com>
2024-03-12 21:39:45 +0330

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
+import { books } from "@/lib/mockData"
+import { motion } from "framer-motion";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
@@ -13,6 +18,29 @@ export default function Home() {
           <section className="content">
             <SideBar />
           </section>
+
+          <div>
+            <h1>ALL BOOKS</h1>
+            <ul>
+              {
+                books.map((book, i) => (
+                  <motion.li key={i}
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ type: 'spring', damping: 50, mass: 0.75 }}
+                    initial={{opacity:0, x:200*(i+1)}}
+                    animate={{opacity:1, x:0}}
+                    key={i}
+                  >
+                    <Link href={"/book/" + book.id}>
+                      <p>{book.title}</p>
+                    </Link>
+                  </motion.li>
+                ))
+              }
+            </ul>
+          </div>
+
         </div>
 
 
