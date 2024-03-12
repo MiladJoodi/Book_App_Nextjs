4df0bec4d1568732848381b89783687932895629
Milad Joodi <winbeta.com@gmail.com>
2024-03-12 21:04:01 +0330

    update

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,10 +1,54 @@
+"use client"
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { motion } from "framer-motion";
+
+
+const MenuList = [
+    {
+      title:"Home",
+      icon: <i className="fa fa-home iconStyle"></i>
+    },
+    {
+      title:"Contact",
+      icon: <i className="fa fa-envelope iconStyle"></i>
+    },
+    {
+      title:"AboutUs",
+      icon: <i className="fa fa-info-circle iconStyle"></i>
+    },
+    {
+      title:"Settings",
+      icon: <i className="fa fa-cog iconStyle"></i>
+    },
+    {
+      title:"Contact",
+      icon: <i className="fa fa-home iconStyle"></i>
+    },
+    {
+      title:"RateUs",
+      icon: <i className="fa fa-star iconStyle"></i>
+    },
+    {
+      title:"Change Password",
+      icon: <i className="fa fa-eye iconStyle"></i>
+    },
+    {
+      title:"Settings",
+      icon: <i className="fa fa-cog iconStyle"></i>
+    }
+  ]
 
 const SideBar = () => {
     return (
-        <div>
-            SideBar
-        </div>
+        <motion.div className="left_container"
+        transition={{ type: 'spring', damping: 18, mass: 0.75 }}
+        initial={{ opacity: 0, x: -10000 }}
+        animate={{ opacity: 1, x: 0 }}
+    >
+        
+        
+    </motion.div>
     );
 }
 
